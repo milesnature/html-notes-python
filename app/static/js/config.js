c3fae091c69ea3dedc719c9ca17718773e5fbcf4
Miles Nature <milesnature@users.noreply.github.com>
2023-01-11 15:13:13 -0800

    Add files via upload

---
+++ app/static/js/config.js
@@ -0,0 +1,10 @@
+const useEncryption = false;
+const isDemo = true;
+const notesDirectory = "notes/";
+const notes = [
+    { "id" : "Node",      "dir" : "Development/Languages/node.html" },
+    { "id" : "Groceries", "dir" : "Health/Nutrition/groceries.html" },
+    { "id" : "Recipies",  "dir" : "Health/Nutrition/recipies.html" },
+    { "id" : "News",      "dir" : "news.html" },
+    { "id" : "To-Do",     "dir" : "to-do.html" }
+];
