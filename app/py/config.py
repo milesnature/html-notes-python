b9d091f1daa12dd0d0b000fd4221096e276aa0b3
Miles Nature <500miles@protonmail.com>
2023-04-21 10:51:39 -0700

    Playing with __name__ and __main__ Error messaging fixes

--- app/py/config.py
+++ app/py/config.py
@@ -1,1 +1,2 @@
-config = {'notes_dir': 'static/notes/', 'is_demo': False, 'use_encryption': True}
+if __name__ != '__main__':
+    config = {'notes_dir': 'static/notes/', 'is_demo': False, 'use_encryption': True}
